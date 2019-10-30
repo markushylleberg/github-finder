922097f1d7d8a4bcbd7e310a96bc928ef1cb44c3
Markus Hylleberg <markushylleberg@gmail.com>
2019-10-30 14:26:18 +0100

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+
+const About = props => {
+    return (
+        <Fragment>
+            <h1>About</h1>
+            <p>App to search github users</p>
+            <p>Version 1.0.0</p>
+        </Fragment>
+    )
+}
+
+
+export default About
